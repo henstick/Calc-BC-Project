b0ee84323c530e0b8c79900f9ca7548b0e13783b
cdavidge23 <connord23@lakesideschool.org>
2023-05-25 10:30:38 -0700

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,28 @@
+from sympy import symbols, diff, solve
+
+# Get the function and variable from the user
+function = input("Enter the function: ")
+variable = input("Enter the variable to optimize with respect to: ")
+
+# Define the symbol
+x = symbols(variable)
+
+# Calculate the derivative of the function
+derivative = diff(function, x)
+
+# Find the critical points by solving the derivative equation
+critical_points = solve(derivative, x)
+
+# Find the values of the function at the critical points
+values = [function.subs(x, point) for point in critical_points]
+
+# Find the maximum and minimum values
+maximum = max(values)
+minimum = min(values)
+
+# Print the results
+print("Derivative:", derivative)
+print("Critical Points:", critical_points)
+print("Function Values at Critical Points:", values)
+print("Maximum Value:", maximum)
+print("Minimum Value:", minimum)
